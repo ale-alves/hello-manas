1cc66806d19db8f5cd2bec02915cb8fa6c3c151e
Alessandra Alves <a.ale_alves@hotmail.com>
2020-11-06 19:54:31 -0300

    Alterei o tema, index.js ok e cipher.js ok

--- cipher.js
+++ cipher.js
@@ -1,7 +1,50 @@
-/*const cipher = {
-  // ...
+
+/* Cifra de Cesar
+
+A  B  C  D  E  F  G  H  I  J  K  L  M  
+0  1  2  3  4  5  6  7  8  9  10 11 12  
+
+N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+13 14 15 16 17 18 19 20 21 22 23 24 25 */
+
+const cipher = {
+  encode(offset, string){
+   
+    let codificada = "";
+    if (offset == null || !string) throw new TypeError("ERRO!");
+
+    for (let i = 0; i < string.length; i++) {
+        let codAsc = string.charCodeAt(i);
+        let offsetIndex = ((codAsc - 65 + offset) % 26) + 65;
+        if (codAsc <= 64 || codAsc >= 91) {
+            offsetIndex = codAsc; 
+        }
+        codificada += String.fromCharCode(offsetIndex);
+    }
+    return codificada;
+ },
+ 
+  decode(offset, string){
+    let decodificada = "";
+    if (offset == null || !string) throw new TypeError("ERRO!");
+
+    for (let i = 0; i < string.length; i++) {
+        let codAsc = string.charCodeAt(i);
+        let offsetIndex = (codAsc - 65 - (offset % 26) + 26) % 26 + 65; 
+        if (codAsc <= 64 || codAsc >= 91) {
+            offsetIndex = codAsc;
+        }
+        decodificada += String.fromCharCode(offsetIndex);
+    }
+    return decodificada;
+ }
+   
 };
+export default cipher;
 
-export default cipher;*/
 
-const cipher = 
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+/*let form = document.querySelector('form');
+form.addEventListener('submit', executaEvento);
+function executaEvento(event){
+event.preventDefault();
+
+
+
+const radios = document.getElementsById('cipher');
+
+
+
+}*/
+
+function clicar(event){
+   let cifrar = document.getElementById('cifrar');
+   let decifrar = document.getElementById('decifrar');
+   let offset = Number(document.getElementById('desloc').value);
+   let string = document.getElementById('msg').value.toUpperCase();
+   let resMsg = document.getElementById('resMsg');
+
+   if (cifrar.checked == true) {
+      let msgCifrar = cipher.encode(offset, string);
+      resMsg.innerHTML = msgCifrar;
+   } else if (decifrar.checked == true) {
+      let msgDecifrar = cipher.decode(offset, string);
+      resMsg.innerHTML = msgDecifrar;
+}
+event.preventDefault();
+}
+
+let submit = document.getElementById('submit');
+submit.addEventListener("click", clicar);
+
+/*form.addEventListener('submit', executaEventoDecifrar);
+function executaEventoDecifrar(event){
+event.preventDefault();
+
+let string = document.getElementById('msg').value.toUpperCase();
+let offset = Number(document.getElementById('desloc').value);
+
+}*/
+
+
