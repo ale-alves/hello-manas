28646f5474a3949afa8d62d0cadbec75b0d62e5e
Alessandra Alves <a.ale_alves@hotmail.com>
2020-11-07 17:08:42 -0300

    Finalizado o projeto e readme

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 import cipher from './cipher.js';
 
-/*let form = document.querySelector('form');
-form.addEventListener('submit', executaEvento);
-function executaEvento(event){
-event.preventDefault();
-
-
-
-const radios = document.getElementsById('cipher');
-
-
-
-}*/
-
 function clicar(event){
    let cifrar = document.getElementById('cifrar');
    let decifrar = document.getElementById('decifrar');
@@ -33,13 +20,6 @@ event.preventDefault();
 let submit = document.getElementById('submit');
 submit.addEventListener("click", clicar);
 
-/*form.addEventListener('submit', executaEventoDecifrar);
-function executaEventoDecifrar(event){
-event.preventDefault();
 
-let string = document.getElementById('msg').value.toUpperCase();
-let offset = Number(document.getElementById('desloc').value);
-
-}*/
 
 
